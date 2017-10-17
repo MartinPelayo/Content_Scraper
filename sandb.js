54b53380729edc0d41e1339f7e39403d65ef411f
Martin <martinconradpelayo@gmail.com>
2017-10-17 09:41:50 -0700

    gets data in correct order and in correct format

---
+++ sandb.js
@@ -0,0 +1,15 @@
+// var osmosis = require('osmosis');
+
+// osmosis.get('http://shirts4mike.com')
+//   .find('div.button a')
+//   // .set('a')
+//   .follow('@href')
+//   .find('ul.products a')
+//   .set('a')
+//   .follow('@href')
+
+//   .find('.price')
+//   .set('.price')
+//   .data(function(results){
+//     console.log(results);
+//   });
